99ea72522b1130a01ac7bcad395e0a5b8ecdbdfa
Юлия Викторовна Ахромова <227890@niuitmo.ru>
2022-02-23 19:37:48 +0300

    Добавьте файлы проекта.

---
+++ kg1.cpp
@@ -0,0 +1,77 @@
+﻿// 2 lines
+
+#include <iostream>
+#include <math.h>
+#include "graphics.h"
+#pragma comment(lib,"graphics.lib")
+
+using namespace std;
+
+
+//#define roundf(x) floor((x) + 0,5f)
+void line_DDA(float x1, float y1, float x2, float y2, COLORREF cColor) {
+    // Целочисленные значения координат начала и конца отрезка,
+    // округлённые до ближайшего целого
+    int iX1 = roundf(x1);
+    int iY1 = roundf(y1);
+    int iX2 = roundf(x2);
+    int iY2 = roundf(y2);
+
+    // Длина и высота линии
+    int deltaX = abs(iX1 - iX2);
+    int deltaY = abs(iY1 - iY2);
+
+    // Считаем минимальное количество итераций, необходимое
+    // для отрисовки отрезка. Выбирая максимум из длины и высоты
+    // линии, обеспечиваем связность линии
+    int length = max(deltaX, deltaY);
+
+    // особый случай, на экране закрашивается ровно один пиксель
+    if (length == 0) {
+        putpixel(iX1, iY1, cColor);
+        return;
+    }
+    // Вычисляем приращения на каждом шаге по осям абсцисс и ординат
+    double dX = (x2 - x1) / length;
+    double dY = (y2 - y1) / length;
+
+    // Начальные значения
+    double x = x1;
+    double y = y1;
+
+    // Основной цикл
+    length++;
+    while (length--) {
+        x += dX;
+        y += dY;
+        putpixel(roundf(x), roundf(y), cColor);
+    }
+
+}
+
+
+
+int main()
+{
+    initwindow(1400, 700); // создаём консольное окно 500 на 500
+
+
+
+    setcolor(14); // жёлтый 14
+    line(50, 100, 100, 300);
+
+    setcolor(11); // cyan 3 or 11
+    line(20, 250, 400, 50);
+
+
+    line_DDA(100, 50, 500, 350, 10); // green 2 or 10
+
+
+
+
+
+    getch(); // чтение одного символа с клавиатуры
+    closegraph(); // освобождает всю память,выделенную под графическую систему, затем восстанавливает экран в режим, который    был   до   вызова   initgraph
+
+    return 0;
+}
